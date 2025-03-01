168c1b91661c0223e10fe4501a6a5bfd064c7d25
CHIK_PUK_PUK 01 <gavrm04@mail.ru>
2025-03-01 03:08:33 +0300

    server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const PORT = 5000;
+const HOST = `http://localhost:${PORT}`;
+
+const products = require("./data")(HOST);
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+// Апи для получение карточки товара
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === parseInt(req.params.id));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send("Продукт не найден");
+  }
+});
+
+app.get("/products", (req, res) => {
+  if (products.length > 0) {
+    res.json(products);
+  } else {
+    res.status(404).send("Ничего нет");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
